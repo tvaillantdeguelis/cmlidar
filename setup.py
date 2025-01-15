5b28e142918111a6924751dd54e5c124b11d0b5c
Thibault Vaillant de Guélis <thibault.vaillantdeguelis@outlook.com>
2025-01-15 14:58:32 +0100

    Add package requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,5 @@ setup(
     name='cmlidar',
     version='v0.0.6',
     packages=find_packages(),
-    install_requires=[
-        # Add dependencies here.
-        # e.g. 'numpy>=1.11.1'
-    ],
+    install_requires=['matplotlib', 'numpy'],
 )
