bb1517af7802cd9d8d226ada72592b6a7a306159
Thibault Vaillant de Guélis <thibault.vaillantdeguelis@outlook.com>
2025-01-15 14:51:16 +0100

    Change version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmlidar',
-    version='0.1',
+    version='v0.0.6',
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
