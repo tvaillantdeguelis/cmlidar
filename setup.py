00a71f9a18cc7ed8f4becdeee2e17f3bcc09c457
Thibault Vaillant de Guélis <thibault.vaillantdeguelis@outlook.com>
2025-02-13 17:13:58 +0100

    Change version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmlidar',
-    version='v1.1.0',
+    version='v1.2.1',
     packages=find_packages(),
     install_requires=['matplotlib', 'numpy'],
     package_data={
