c9bff8eb7feb41e216a12efc308561455dcf968b
Thibault Vaillant de Guélis <thibault.vaillantdeguelis@outlook.com>
2025-02-13 17:09:58 +0100

    Correct CR colormap and rename cmaps

--- cmlidar/cm.py
+++ cmlidar/cm.py
@@ -5,7 +5,7 @@ Created by Thibault Vaillant de Guélis
 2024-09-02
 
 Edited by Rob Ryan
-2025-01-16
+2025-02-06
 '''
 
 import os
@@ -15,90 +15,142 @@ import matplotlib
 
 try:
     from matplotlib import colormaps
-    register_cmap = lambda name, cmap: colormaps.register(cmap, name=name)
+    _register_cmap = lambda name, cmap: colormaps.register(cmap, name=name)
 except ImportError:
     # PendingDeprecationWarning from matplotlib 3.6
     from matplotlib.cm import register_cmap
-
+    _register_cmap = lambda name, cmap: register_cmap(cmap, name=name)
 
 # Location of rgb files
 _datadir = os.path.join(os.path.dirname(__file__), 'rgb')
 
-# List of colormap names
-_cmapnames = ['backscatter', 'backscatter_discrete', 'backscatter_continuous', 
-              'depolarization', 'depolarization_discrete',
-              'colorratio', 'colorratio_discrete']
 
-# Initialize dictionaries to contain cmap and norm
-cmap_d = dict()
-norm_d = dict()
+def _make_matplotlib_linearsegcmap_from_csv_file(cmapname: str, datadir: str, reverse=False) -> matplotlib.colors.LinearSegmentedColormap:
+    """Make a matplotlib colormap from a CSV file containing the list a RGB 0-255 color values.
 
-def _make_mpl_lscm(cmapname, rgb_0_1, rgb_over, rgb_under, reverse=False):
-    over_color = rgb_over
-    under_color = rgb_under
+    Args:
+        cmapname (str): name of the colormap
+        datadir (str): directory containing the file with RGB 0-255 color values
+        reverse (bool, optional): if True, reverse the colormap. Defaults to False.
+
+    Returns:
+        matplotlib.colors.LinearSegmentedColormap: matplotlib colormap
+    """
+    # Get list of 0-255 RGB colors
+    csv_filename = os.path.join(datadir, cmapname + '-rgb.csv')
+    rgb_0_255 = np.genfromtxt(csv_filename, delimiter=',')
+    
     if reverse:
+        # Reverse the colormap
         cmapname = cmapname + '_r'
-        rgb_0_1 = rgb_0_1[::-1, :]
-        over_color = rgb_under
-        under_color = rgb_over
-    # Add colormap to dictionary
+        rgb_0_255 = rgb_0_255[::-1, :]
+
+    # Convert 0-255 color values to 0.0-1.0 float values
+    rgb_0_1 = rgb_0_255/255.
+
+    # Extract first and last colors of the list as over and under colors
+    under_color = rgb_0_1[0]
+    cmap_colors = rgb_0_1[1:-1,:]
+    over_color = rgb_0_1[-1]
+
+    # Make a matplotlib LinearSegmentedColormap
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-        cmapname, rgb_0_1, N=rgb_0_1.shape[0])
+        cmapname, cmap_colors, N=cmap_colors.shape[0])
     cmap.name = cmapname
     cmap.set_under(under_color)
     cmap.set_over(over_color)
-    # register colormap for matplotlib to use
-    register_cmap(cmapname, cmap)
-    return cmap, cmapname
 
-def _getrgb_0_1_over_under(datadir, cmapname):
-    # Load RGB values
-    rgb_0_255 = np.genfromtxt(os.path.join(datadir, cmapname + '-rgb.csv'), delimiter=',')
-    # Normalize 0-255 RGB values to 0-1
-    rgb_0_1 = rgb_0_255/255
-    
-    # Extract under and over colors
-    over_color = rgb_0_1[-1]
-    under_color = rgb_0_1[0]
-    rgb_0_1 = rgb_0_1[1:-1,:]
+    # Register colormap for matplotlib to use (e.g., "cmap=<cmapname>")
+    _register_cmap(cmapname, cmap) 
 
-    return rgb_0_1, over_color, under_color
+    return cmap
 
-# Add colormaps and reversed to dictionary
-for cmapname in _cmapnames:
-    rgb_0_1, over_color, under_color = _getrgb_0_1_over_under(_datadir, cmapname)
-    cmap, cmapname = _make_mpl_lscm(cmapname, rgb_0_1, over_color, under_color)
-    cmap_d[cmapname] = cmap
-    cmap_r, cmapname_r = _make_mpl_lscm(cmapname, rgb_0_1, over_color, under_color, reverse=True)
-    cmap_d[cmapname_r] = cmap_r
 
-# Boundary arrays
+_cmap_name = 'backscatter'
+backscatter = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A continuous perceptually uniform colormap, with over and under range colors, specifically 
+selected to highlight cloud, aerosol and molecular particulate atmospheric features in 
+attenuated backscatter data such as is provided by the CALIOP lidar and other similar elastic 
+backscatter lidars."""
+backscatter_r = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir, reverse=True)
+"""A reversed version of the 'backscatter' colormap."""
+
+_cmap_name = 'backscatter_18'
+backscatter_18 = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A discrete colormap with 18 colors, including over and under range colors, specifically 
+selected to highlight cloud, aerosol and molecular particulate atmospheric features 
+in attenuated backscatter data such as is provided by the CALIOP lidar and other similar elastic 
+backscatter lidars."""
 BACKSCATTER_DISCRETE_BOUNDS = [
-    1e-5,
-    1e-4, 3e-4, 6e-4,
-    1e-3, 1.5e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 8e-3,
-    1e-2, 1.5e-2, 2e-2, 3e-2, 5e-2
-    ]
-norm_d['backscatter_discrete_norm'] = matplotlib.colors.BoundaryNorm(
+    1e-5, 1e-4, 3e-4, 6e-4, 1e-3, 1.5e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 8e-3,
+    1e-2, 1.5e-2, 2e-2, 3e-2, 5e-2]
+"""Boundary limits specifically designed to highlight particulate atmospheric 
+features in the attenuated backscatter data provided by the CALIOP lidar and other similar 
+elastic backscatter lidars."""
+backscatter_18_norm = matplotlib.colors.BoundaryNorm(
     BACKSCATTER_DISCRETE_BOUNDS, len(BACKSCATTER_DISCRETE_BOUNDS)-1)
+"""A matplotlib.colors.BoundaryNorm object initialized with the BACKSCATTER_DISCRETE_BOUNDS 
+and designed to use with the 'backscatter_18' colormap."""
 
-DEPOLARIZATION_DISCRETE_BOUNDS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-norm_d['depolarization_discrete_norm'] = matplotlib.colors.BoundaryNorm(
-    DEPOLARIZATION_DISCRETE_BOUNDS, len(DEPOLARIZATION_DISCRETE_BOUNDS)-1)
+_cmap_name = 'backscatter_242'
+backscatter_242 = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A continuous colormap, with over and under range colors, specifically selected 
+to highlight cloud, aerosol and molecular particulate atmospheric features in 
+attenuated backscatter data such as is provided by the CALIOP lidar and other similar elastic 
+backscatter lidars."""
+_NB_COLORS_BETWEEN_MID_DISCRETE_BIN = 15
+_bounds = np.array(())
+for _i in range(len(BACKSCATTER_DISCRETE_BOUNDS)-1): # color of discrete bounds at the mid value of the range bin
+    _bounds = np.append(_bounds, np.linspace(BACKSCATTER_DISCRETE_BOUNDS[_i], BACKSCATTER_DISCRETE_BOUNDS[_i+1], _NB_COLORS_BETWEEN_MID_DISCRETE_BIN+1)[:-1])
+_bounds = np.append(_bounds, BACKSCATTER_DISCRETE_BOUNDS[-1])
+_backscatter_continuous_bounds = _bounds.tolist()
+backscatter_242_norm = matplotlib.colors.BoundaryNorm(
+    _backscatter_continuous_bounds, len(_backscatter_continuous_bounds)-1)
+"""A matplotlib.colors.BoundaryNorm object initialized with the DEPOLARIZATION_DISCRETE_BOUNDS 
+and designed to use with the 'backscatter_242' colormap."""
 
-COLORRATIO_DISCRETE_BOUNDS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
-norm_d['colorratio_discrete_norm'] = matplotlib.colors.BoundaryNorm(
+_cmap_name = 'depol'
+depol = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A continuous perceptually uniform colormap, with over and under range colors, specifically 
+selected to highlight cloud, aerosol and molecular particulate atmospheric features in 
+depolarization ratio data such as is provided by the CALIOP lidar and other similar 
+elastic backscatter lidars."""
+depol_r = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir, reverse=True)
+"""A reversed version of the 'depol' colormap."""
+
+_cmap_name = 'depol_8'
+depol_8 = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A discrete colormap with 8 colors, including over and under range colors, specifically 
+selected to highlight cloud, aerosol and molecular particulate atmospheric features 
+in depolarization ratio data such as is provided by the CALIOP lidar and other 
+similar elastic backscatter lidars."""
+DEPOL_DISCRETE_BOUNDS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+"""Boundary limits specifically designed to highlight particulate atmospheric 
+features in the depolarization ratio data provided by the CALIOP lidar and other 
+similar elastic backscatter lidars."""
+depol_8_norm = matplotlib.colors.BoundaryNorm(
+    DEPOL_DISCRETE_BOUNDS, len(DEPOL_DISCRETE_BOUNDS)-1)
+"""A matplotlib.colors.BoundaryNorm object initialized with the DEPOLARIZATION_DISCRETE_BOUNDS 
+and designed to use with the 'depol_8' colormap."""
+
+_cmap_name = 'colorratio'
+colorratio = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A continuous perceptually uniform colormap, with over and under range colors, to highlight 
+particulate atmospheric features in the 1064 nm/532 nm color ratio data such as 
+are provided by the CALIOP lidar and other similar elastic backscatter lidars."""
+colorratio_r = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir, reverse=True)
+"""A reversed version of the 'colorratio' colormap."""
+
+_cmap_name = 'colorratio_9'
+colorratio_9 = _make_matplotlib_linearsegcmap_from_csv_file(_cmap_name, _datadir)
+"""A discrete colormap of 9 colors, including over and under range colors, to highlight 
+particulate atmospheric features in the 1064 nm/532 nm color ratio data such as 
+are provided by the CALIOP lidar and other similar elastic backscatter lidars."""
+COLORRATIO_DISCRETE_BOUNDS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.6]
+"""Boundary limits specifically designed to highlight particulate atmospheric 
+features in the 1064 nm/532 nm color ratio data such as are provided by the CALIOP 
+lidar and other similar elastic backscatter lidars."""
+colorratio_9_norm = matplotlib.colors.BoundaryNorm(
     COLORRATIO_DISCRETE_BOUNDS, len(COLORRATIO_DISCRETE_BOUNDS)-1)
-
-NB_COLORS_BETWEEN_MID_DISCRETE_BIN = 15
-bounds = np.array(())
-for i in range(len(BACKSCATTER_DISCRETE_BOUNDS)-1): # color of discrete bounds at the mid value of the range bin
-    bounds = np.append(bounds, np.linspace(BACKSCATTER_DISCRETE_BOUNDS[i], BACKSCATTER_DISCRETE_BOUNDS[i+1], NB_COLORS_BETWEEN_MID_DISCRETE_BIN+1)[:-1])
-bounds = np.append(bounds, BACKSCATTER_DISCRETE_BOUNDS[-1])
-BACKSCATTER_CONTINUOUS_BOUNDS = bounds.tolist()
-norm_d['backscatter_continuous_norm'] = matplotlib.colors.BoundaryNorm(
-    BACKSCATTER_CONTINUOUS_BOUNDS, len(BACKSCATTER_CONTINUOUS_BOUNDS)-1)
-
-# Make cmaps and norms available to call
-locals().update(cmap_d)
-locals().update(norm_d)
+"""A matplotlib.colors.BoundaryNorm object initialized with the COLORRATIO_DISCRETE_BOUNDS 
+and designed to use with the 'colorratio_9' colormap."""
--- examples/plot.py
+++ examples/plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FixedLocator, LogLocator
 import cmlidar
 
+
 def backscatter_cbar_labels(cbar):
     cbar.ax.yaxis.set_major_locator(LogLocator(numticks=15))
     cbar.ax.tick_params(which='both', labelright=False)
@@ -19,42 +20,66 @@ def backscatter_cbar_labels(cbar):
     for j, bound in enumerate(c_bar_major_values):
         cbar.ax.text(2.5, bound, cbar_major_label[j], va='center', fontsize=8)
 
-# Generate random data for the subplots
-np.random.seed(42)  # For reproducibility
-random_depolarization_data = np.random.uniform(0, 0.8, (10, 10)) 
-random_colorratio_data = np.random.uniform(0, 1.6, (10, 10))   
-random_backscatter_data = np.random.uniform(1.0e-6, 6.0e-2, (10, 10))
 
-# Create a figure
-fig, axes = plt.subplots(4, 1, figsize=(8, 15), dpi=300)
+if __name__ == "__main__":
+    # Generate random data for the subplots
+    np.random.seed(42)  # For reproducibility
+    random_depolarization_data = np.random.uniform(0, 0.8, (10, 10)) 
+    random_colorratio_data = np.random.uniform(0, 1.6, (10, 10))   
+    random_backscatter_data = np.random.uniform(1.0e-6, 6.0e-2, (10, 10))
 
-ax = axes[0]
-p = ax.pcolormesh(random_depolarization_data, cmap="depolarization_discrete", 
-                        norm=cmlidar.cm.depolarization_discrete_norm)
-cbar = plt.colorbar(p, ax=ax, extend='both')
-ax.set_title('depolarization_discrete')
+    # Create a figure
+    fig, axes = plt.subplots(7, 1, figsize=(8, 28), dpi=300)
 
-ax = axes[1]
-p = ax.pcolormesh(random_colorratio_data, cmap="colorratio_discrete", 
-                        norm=cmlidar.cm.colorratio_discrete_norm)
-cbar = plt.colorbar(p, ax=ax, extend='both')
-ax.set_title('colorratio_discrete')
+    ax = axes[0]
+    p = ax.pcolormesh(random_depolarization_data, 
+                    cmap=cmlidar.cm.depol_8, 
+                    norm=cmlidar.cm.depol_8_norm)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    ax.set_title('depol_8')
 
-ax = axes[2]
-p = ax.pcolormesh(random_backscatter_data, cmap="backscatter_discrete", 
-                        norm=cmlidar.cm.backscatter_discrete_norm)
-cbar = plt.colorbar(p, ax=ax, extend='both')
-backscatter_cbar_labels(cbar)
-ax.set_title('backscatter_discrete')
+    ax = axes[1]
+    p = ax.pcolormesh(random_colorratio_data, 
+                    cmap=cmlidar.cm.colorratio_9, 
+                    norm=cmlidar.cm.colorratio_9_norm)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    ax.set_title('colorratio_9')
 
-ax = axes[3]
-p = ax.pcolormesh(random_backscatter_data, cmap="backscatter_continuous", 
-                        norm=cmlidar.cm.backscatter_continuous_norm)
-cbar = plt.colorbar(p, ax=ax, extend='both')
-backscatter_cbar_labels(cbar)
-ax.set_title('backscatter_continuous')
+    ax = axes[2]
+    p = ax.pcolormesh(random_backscatter_data, 
+                    cmap=cmlidar.cm.backscatter_18, 
+                    norm=cmlidar.cm.backscatter_18_norm)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    backscatter_cbar_labels(cbar)
+    ax.set_title('backscatter_18')
 
-plt.tight_layout()
+    ax = axes[3]
+    p = ax.pcolormesh(random_backscatter_data, 
+                    cmap=cmlidar.cm.backscatter_242, 
+                    norm=cmlidar.cm.backscatter_242_norm)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    backscatter_cbar_labels(cbar)
+    ax.set_title('backscatter_242')
 
-# Save figure
-plt.savefig('./examples/colorbars.png')
+    ax = axes[4]
+    p = ax.pcolormesh(random_depolarization_data, 
+                    cmap=cmlidar.cm.depol)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    ax.set_title('depol')
+
+    ax = axes[5]
+    p = ax.pcolormesh(random_colorratio_data, 
+                    cmap=cmlidar.cm.colorratio)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    ax.set_title('colorratio')
+
+    ax = axes[6]
+    p = ax.pcolormesh(random_backscatter_data, 
+                    cmap=cmlidar.cm.backscatter)
+    cbar = plt.colorbar(p, ax=ax, extend='both')
+    ax.set_title('backscatter')
+
+    plt.tight_layout()
+
+    # Save figure
+    plt.savefig('./examples/colorbars.png')
